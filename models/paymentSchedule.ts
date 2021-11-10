b02b8a74a9db38badc4916811150827acfdefb68
PriyankaChitare1 <71915753+PriyankaChitare1@users.noreply.github.com>
2021-11-10 07:44:39 +0530

    FrontEndLoan

---
+++ models/paymentSchedule.ts
@@ -0,0 +1,9 @@
+export class PaymentSchedule {
+    public paymentId:number;
+    public loanId: string;
+    public paymentDate: string;
+    public principal: number;
+    public projectedInterest: number;
+    public paymentStatus: string;
+    public paymentAmount: number;
+  }
